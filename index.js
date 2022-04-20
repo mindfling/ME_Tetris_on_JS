5e5e0b6ccf92d6200ff638d356591abf72f1937c
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-04-20 18:56:33 +0000

    add main files

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+alert('container');
+console.log('Hello this is Tetris on JS');
+
+
+const SIZE_BLOCK = 30;
+const container = document.querySelector('.container');
+console.log('container: ', container);
+
+
+const canvas = document.createElement('canvas');
+console.log('canvas: ', canvas);
+
+
